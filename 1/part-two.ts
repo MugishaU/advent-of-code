bcab4dfebd93787def3ac0415e84e2f9b99f02d1
MugishaU <muwiragiye123@gmail.com>
2023-12-03 20:24:26 +0000

    day 1 part 2 attempt

--- 1/part-two.ts
+++ 1/part-two.ts
@@ -2,40 +2,136 @@ import events from 'events';
 import * as fs from 'fs';
 import * as rd from 'readline';
 
+function isNum(letter: string) {
+  return !isNaN(Number(letter));
+}
+
+function calibrationValue(line: string) {
+  const values: string[] = [];
+  const spelledDigits = {
+    three: ['one', 'two', 'six', 'ten'],
+    four: ['four', 'five', 'nine'],
+    five: ['three', 'seven', 'eight']
+  };
+
+  const dictionary: Record<string, string> = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9'
+  };
+
+  let leftPointer = 0;
+  let rightPointer = 0;
+  let tempSubstring = '';
+
+  while (leftPointer < line.length) {
+    switch (tempSubstring.length) {
+      // Check if temporary substring matches a digit
+      case 3:
+        if (spelledDigits.three.includes(tempSubstring)) {
+          values.push(tempSubstring);
+          leftPointer = rightPointer;
+          tempSubstring = '';
+        }
+        break;
+      case 4:
+        if (spelledDigits.four.includes(tempSubstring)) {
+          values.push(tempSubstring);
+          leftPointer = rightPointer;
+          tempSubstring = '';
+        }
+        break;
+      case 5:
+        if (spelledDigits.five.includes(tempSubstring)) {
+          values.push(tempSubstring);
+          leftPointer = rightPointer;
+          tempSubstring = '';
+        }
+        break;
+      default:
+        break;
+    }
+
+    if (isNum(line[leftPointer])) {
+      // If: left pointer is a number, add to values and move left pointer to next position to restart
+      values.push(line[leftPointer]);
+      leftPointer++;
+      rightPointer = leftPointer;
+    } else if (
+      tempSubstring.length == 6 ||
+      rightPointer > line.length ||
+      isNum(line[rightPointer])
+    ) {
+      /**
+       * If:
+       *  - temporary substring is over 5 digits long without a match
+       *  - right pointer has gone past end of the line
+       *  - right pointer has landed on a number before a match
+       *
+       * Then:
+       *  - move left pointer to next position to restart
+       *  - clear temporary substring
+       */
+      leftPointer++;
+      rightPointer = leftPointer;
+      tempSubstring = '';
+    } else {
+      // Else: Add right pointer character to temporary substring and move right pointer to next position
+      tempSubstring += line[rightPointer];
+      rightPointer++;
+    }
+  }
+
+  const convertedValues = values.map(string => {
+    if (string.length > 1) {
+      return dictionary[string];
+    } else {
+      return string;
+    }
+  });
+
+  // console.log(values);
+  // console.log(convertedValues);
+
+  if (convertedValues.length > 1) {
+    const x = convertedValues.shift();
+    const y = convertedValues.pop();
+    //@ts-expect-error It won't be undefined
+    return Number(x + y);
+  }
+
+  return Number(convertedValues[0] + convertedValues[0]);
+}
+
 async function handleInput(filename: string) {
   try {
+    let total = 0;
+    let lines = 0;
+    let count = 1;
     const rl = rd.createInterface({
       input: fs.createReadStream(filename),
       crlfDelay: Infinity
     });
 
     rl.on('line', line => {
-      console.log(line);
+      const lineAmount = calibrationValue(line);
+      total = total + lineAmount;
+      count++;
     });
 
+    console.log(total);
     await events.once(rl, 'close');
+    return total;
+    console.log(lines);
   } catch (err) {
     console.error(err);
   }
 }
 
-function calibrationValue(line: string) {
-  const words = [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-    'ten'
-  ];
-  for (const i of line) {
-    console.log(i);
-  }
-}
-
-// handleInput('input.txt');
-calibrationValue('xtwone3four');
+handleInput('1/input.txt').then(res => console.log(res));
