c9fab2c99f8cd1c83ea6b90c4bea76f0e54941ff
MugishaU <muwiragiye123@gmail.com>
2023-12-07 01:43:08 +0000

    day 3 part 2

---
+++ 3/part-two.ts
@@ -0,0 +1,117 @@
+import events from 'events';
+import * as fs from 'fs';
+import * as rd from 'readline';
+import { isNum } from '../0_utils/isNum';
+
+async function handleInput(filename: string): Promise<string[][]> {
+  try {
+    const engine: string[][] = [];
+    const rl = rd.createInterface({
+      input: fs.createReadStream(filename),
+      crlfDelay: Infinity
+    });
+
+    rl.on('line', line => {
+      const row = line.split('');
+      engine.push(row);
+    });
+
+    await events.once(rl, 'close');
+    return engine;
+  } catch (err) {
+    console.error(err);
+    throw new Error();
+  }
+}
+
+function calculate(engine: string[][]) {
+  let total = 0;
+  let gears: Record<string, number[]> = {};
+
+  for (const [rowIdx, row] of engine.entries()) {
+    // Loop through each row
+    let tempNumber = '';
+    // let numberToggle = false; // Toggle to track when a new number is first encountered
+    const adjIdxs: Set<string> = new Set();
+
+    for (const [itemIdx, item] of row.entries()) {
+      // Loop through each item in a row
+      if (isNum(item)) {
+        // numberToggle = true; // Toggle switch to get total number
+        tempNumber += item; // Add first digit to tempNumber
+
+        // Add adjacent indices value to check later.
+        // Order: top left, top mid, top right, mid left,
+        // mid right, bottom left, bottom mid, bottom right
+
+        const maybeIdxs = [
+          [rowIdx - 1, itemIdx - 1],
+          [rowIdx - 1, itemIdx],
+          [rowIdx - 1, itemIdx + 1],
+          [rowIdx, itemIdx - 1],
+          [rowIdx, itemIdx + 1],
+          [rowIdx + 1, itemIdx - 1],
+          [rowIdx + 1, itemIdx],
+          [rowIdx + 1, itemIdx + 1]
+        ];
+
+        // Remove indexes that aren't possible
+        // Order: above engine, below engine, left of engine, right of engine
+        const idxs = maybeIdxs.filter(
+          ([maybeRow, maybeItem]) =>
+            maybeRow >= 0 &&
+            maybeRow < engine.length &&
+            maybeItem >= 0 &&
+            maybeItem < row.length
+        );
+
+        const strIdxs = idxs.map(idx => idx.join(',')); // Map to string to make set work better
+        strIdxs.forEach(strIdx => adjIdxs.add(strIdx)); // Add each value to set
+
+        // Edge case, number is at the end of a row
+        if (itemIdx === row.length - 1) {
+          const idxs = Array.from(adjIdxs);
+          idxs.forEach(idx => {
+            const [x, y] = idx.split(',').map(i => Number(i));
+            if (engine[x][y] === '*') {
+              if (gears[idx]) {
+                gears[idx].push(Number(tempNumber));
+              } else {
+                gears[idx] = [Number(tempNumber)];
+              }
+            }
+          });
+
+          tempNumber = '';
+          adjIdxs.clear();
+        }
+      } else if (tempNumber.length > 0) {
+        const idxs = Array.from(adjIdxs);
+        idxs.forEach(idx => {
+          const [x, y] = idx.split(',').map(i => Number(i));
+          if (engine[x][y] === '*') {
+            if (gears[idx]) {
+              gears[idx].push(Number(tempNumber));
+            } else {
+              gears[idx] = [Number(tempNumber)];
+            }
+          }
+        });
+
+        tempNumber = '';
+        adjIdxs.clear();
+      }
+    }
+  }
+
+  for (const key in gears) {
+    if (gears[key].length === 2) {
+      const [x, y] = gears[key];
+      total += Number(x) * Number(y);
+    }
+  }
+
+  return total;
+}
+
+handleInput('3/input.txt').then(x => console.log(calculate(x)));
