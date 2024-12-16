79f934d6b334eed89993882f0eb76e64d41d13e4
MugishaU <mugisha@lapse.com>
2024-12-16 16:56:26 +0000

    day 3 part 2

--- 03/index.ts
+++ 03/index.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 const INPUT_PATH = path.join(__dirname, "input.txt");
 
 const trigger = async () => {
-  const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+  const regex = /(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))/g;
   const rows = await readTxtLines(path.join(__dirname, "input.txt"));
   const matches = rows.map((line) => line.matchAll(regex));
   const muls = [];
@@ -16,16 +16,33 @@ const trigger = async () => {
   }
 
   //mul(xxx,xxx)
-  const mulNums = muls.map((mul) =>
-    mul
+  const mulNums = muls.map((mul) => {
+    if (mul === "do()" || mul === "don't()") {
+      return mul;
+    }
+
+    return mul
       .substring(4, mul.length - 1)
       .split(",")
-      .map(Number)
-  );
+      .map(Number);
+  });
 
   let total = 0;
+  let count = true;
 
-  mulNums.forEach((mulNum) => (total += mulNum[0] * mulNum[1]));
+  mulNums.forEach((mulNum) => {
+    if (mulNum == "do()") {
+      count = true;
+      return;
+    }
+
+    if (mulNum == "don't()") {
+      count = false;
+      return;
+    }
+
+    if (count) total += mulNum[0] * mulNum[1];
+  });
   console.log(total);
 };
 
